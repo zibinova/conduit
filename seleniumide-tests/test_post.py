6c6352c422bf1196ac7d4b2d3163335a84d3f1fb
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-06-21 18:08:01 +0200

    updating test_post.py files

--- seleniumide-tests/test_post.py
+++ seleniumide-tests/test_post.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+
 class TestPost():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
