0e69698caa2f96bf5f0e9343e49593f8b6321fda
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-07-30 22:09:07 +0200

    update

--- Vizsgaremek-tests/test_1000_policy.py
+++ Vizsgaremek-tests/test_1000_policy.py
@@ -1,5 +1,6 @@
 #  CON_TC_1000_policy cookie consent giving check
-def test_reg_1000():
+
+def test_1000_policy():
     from selenium import webdriver
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
@@ -7,6 +8,7 @@ def test_reg_1000():
     from webdriver_manager.chrome import ChromeDriverManager
     import time
     from selenium.common.exceptions import NoSuchElementException
+    import pytest
 
     driver = webdriver.Chrome(ChromeDriverManager().install())
     # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)  # headless mode
@@ -15,29 +17,18 @@ def test_reg_1000():
     # options.add_argument('--headless')
     # options.add_argument('--disable-gpu')
 
-    try:
-        driver.get("http://localhost:1667")
-        time.sleep(5)
-        assert driver.find_element_by_id('cookie-policy-panel').is_displayed()
-        cookie_accept_button = driver.find_element_by_xpath("//div[@class='cookie__bar__buttons']/button[2]")
-        cookie_accept_button.click()
+    driver.get("http://localhost:1667")
+    time.sleep(2)
+    assert driver.find_element_by_id('cookie-policy-panel').is_displayed()
+    time.sleep(2)
+    cookie_accept_button = driver.find_element_by_xpath("//div[@class='cookie__bar__buttons']/button[2]")
+    cookie_accept_button.click()
 
-        try:
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "driver.find_element_by_id('cookie-policy-panel')")))
-            not_found = False
+    # checking if panel has disappeared:
+    assert not len(driver.find_elements_by_id('cookie-policy-panel'))
 
-        except:
-            not_found = True
-        assert not_found
 
-        driver.refresh()
-        try:
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "driver.find_element_by_id('cookie-policy-panel')")))
-            not_found = False
 
-        except:
-            not_found = True
-        assert not_found
 
-    finally:
-        driver.close()
+
+
